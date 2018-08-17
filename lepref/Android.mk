10832c179d1749cb12a32426516b420f417613cf
Lucchetto <lucchetto.tie@live.com>
2018-08-17 17:24:21 +0200

    Selinux permissive
    Change-Id: If7f9a493363b729495800ac1dd55d1efb7dbde2d

--- lepref/Android.mk
+++ lepref/Android.mk
@@ -6,9 +6,9 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := LePref
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_VENDOR_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
